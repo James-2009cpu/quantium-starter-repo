a4e50a9d66206ee10dc50e17bb24e46da0a22e69
James McFarlane <jmcfarlane450@gmail.com>
2026-06-16 23:11:49 +0100

    Build sales pipeline and Dash visualisation

--- data/analysis.py
+++ data/analysis.py
@@ -1,10 +1,23 @@
 import pandas as pd
 
-# Load all CSV files
-df1 = pd.read_csv("data/file1.csv")
-df2 = pd.read_csv("data/file2.csv")
-df3 = pd.read_csv("data/file3.csv")
+import pandas as pd
 
+df1 = pd.read_csv("data/daily_sales_data_0.csv")
+df2 = pd.read_csv("data/daily_sales_data_1.csv")
+df3 = pd.read_csv("data/daily_sales_data_2.csv")
+
+df = pd.concat([df1, df2, df3])
+
+df = df[df["product"] == "Pink Morsels"]
+
+df["Sales"] = df["quantity"] * df["price"]
+
+output = df[["Sales", "date", "region"]]
+output.columns = ["Sales", "Date", "Region"]
+
+output.to_csv("sales_output.csv", index=False)
+
+print("DONE")
 # Combine them
 df = pd.concat([df1, df2, df3])
 
